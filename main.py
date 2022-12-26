ce1f1411f627fb5bfdcd2160471dcaf6035a6003
Hai Pham <haiphamnguyenhoang@gmail.com>
2022-12-26 21:07:10 +0800

    update

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import pandas
-from datetime import datetime, timedelta
 import yfinance as yf
 
 DEBUG = 0
 
-def get_end_date_str(start_date, days_after=10):
-    if DEBUG: print(start_date)
-    # Date string format: yyyy-mm-dd
-    date_object = datetime.strptime(start_date, '%Y-%m-%d').date()
-    td = timedelta(days=days_after)
-    return str(date_object+td)
-
-def fetch_stock_data_from_yfinance(stock_symbol, start_date, end_date):
+def fetch_stock_data_from_yfinance(stock_symbol):
     tickerData = yf.Ticker(stock_symbol)
-    tickerDf = tickerData.history(period='1mo', start=start_date, end=end_date)
-    return tickerDf
+    info = tickerData.info
+    tickerDf = tickerData.history(period='max')
+    return info, tickerDf
 
 def process_stock_data(tickerDf):
     # init
-    result = []
+    result = {}
+
     # get first 5 records
     df =  tickerDf.head(5)
-    # get open price
+
+    # get IPO date
+    ipo_date_raw = df.index[0].to_pydatetime()
+    result.update({'IPO_Date': ipo_date_raw.strftime('%Y-%m-%d')})
+    result.update({'IPO_Timezone': str(ipo_date_raw.tzinfo)})
+
+    # get IPO open price
     item = df.iloc[0]
     if DEBUG: print(item.keys())
-    result.append(float(item.Open))
+    result.update({'Open_1': float(item.Open)})
+
     # get next 4 days close price
     for i in range(1, 5):
+        key = 'Close_{}'.format(i+1)
         item = df.iloc[i]
-        result.append(float(item.Close))
+        result.update({key: float(item.Close)})
     
     return result
 
@@ -38,26 +40,27 @@ excel_data_df = pandas.read_excel('input.xlsx', sheet_name='Sheet1')
 
 for idx in excel_data_df.index:
     # input
-    symbol   = excel_data_df.loc[idx, "Symbol"]
-    ipo_date = excel_data_df.loc[idx, "IPO_Date"]
-    
-    # get end_date
-    end_date = get_end_date_str(ipo_date)
-    if DEBUG: print(end_date)
+    symbol   = excel_data_df.loc[idx, 'Symbol']
 
     # get ticker dataframe
-    tickerDf = fetch_stock_data_from_yfinance(symbol, ipo_date, end_date)
+    info, tickerDf = fetch_stock_data_from_yfinance(symbol)
 
+    # get company's info
+    excel_data_df.loc[idx, 'Name']      = info['longName']
+    
     # process the first 5 days after IPO
-    prices = process_stock_data(tickerDf)
+    stock_data = process_stock_data(tickerDf)
 
-    excel_data_df.loc[idx, "Open_1"]  = prices[0]
-    excel_data_df.loc[idx, "Close_2"] = prices[1]
-    excel_data_df.loc[idx, "Close_3"] = prices[2]
-    excel_data_df.loc[idx, "Close_4"] = prices[3]
-    excel_data_df.loc[idx, "Close_5"] = prices[4]
+    excel_data_df.loc[idx, 'IPO_Date']      = stock_data['IPO_Date']
+    excel_data_df.loc[idx, 'IPO_Timezone']  = stock_data['IPO_Timezone']
+
+    excel_data_df.loc[idx, 'Open_1']  = str(stock_data['Open_1'])
+    excel_data_df.loc[idx, 'Close_2'] = str(stock_data['Close_2'])
+    excel_data_df.loc[idx, 'Close_3'] = str(stock_data['Close_3'])
+    excel_data_df.loc[idx, 'Close_4'] = str(stock_data['Close_4'])
+    excel_data_df.loc[idx, 'Close_5'] = str(stock_data['Close_5'])
 
     if DEBUG: print(excel_data_df.iloc[idx])
 
 print(excel_data_df)
-excel_data_df.to_excel('output.xlsx', float_format="%.6f", index=False)
+excel_data_df.to_excel('output.xlsx', index=False)
